b4a76de8200259e05cb98cd3fead187e762915de
DavidG159 <gaspardavid235@gmail.com>
2025-06-18 13:19:23 -0600

    fixed a little bit of css and started testing the keyarrows

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let box = document.querySelector('.mybox');
+
+
+window.addEventListener("keydown", function (e) {
+
+    if (!e.repeat) {
+        console.log(`Key ${e.key} is pressed`);
+    } else {
+        console.log(`Key ${e.key} is repeating`);
+    }
+
+
+});
